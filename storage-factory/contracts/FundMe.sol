3ce6d70de76072f71dbe70984550358219b256a1
apotatocn <huxiaopeng666@qq.com>
2024-07-31 18:56:07 +0800

    add pic cover

--- storage-factory/contracts/FundMe.sol
+++ storage-factory/contracts/FundMe.sol
@@ -1,9 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.8;
 
-
 import {AggregatorV3Interface} from "@chainlink/contracts@1.2.0/src/v0.8/shared/interfaces/AggregatorV3Interface.sol";
-
 
 // Get funds from users（从用户处获得资金）
 // Withdraw funds（提取资金）
@@ -12,6 +10,7 @@ contract FundMe {
     uint256 public minimumUsd = 50;
 
     // 从用户处获得资金
+    // 20000000000000000
     // payable修饰符表示改函数是一个合约账户的函数，就像账户钱包地址一样拥有余额value
     function fund() public payable {
         // Want to be able to set a minimum fund amount in USD
@@ -22,25 +21,40 @@ contract FundMe {
         // msg.gas = gas费用
         // msg.sender = ？
         // msg.value = 参数
-        require(msg.value > minimumUsd, "Didn't send enough!");
+        require(getConversionRate(msg.value) > minimumUsd, "Didn't send enough!");
         // 如何能向这个合约转入ETH
     }
 
-    function getPrice() public {
+    function getPrice() public view returns (uint256) {
         // ABI
         // Address
         // ETH/USD: 0x694AA1769357215DE4FAC081bf1f309aDC325306
         // BTC/USD: 0x1b44F3514812d835EB1BDB0acB33d3fA3351Ee43
-        // 
+        AggregatorV3Interface priceFeed = AggregatorV3Interface(
+            0x694AA1769357215DE4FAC081bf1f309aDC325306
+        );
+        (, int256 price, , , ) = priceFeed.latestRoundData();
+        // ETH in terms of USD
+        // 3000.00000000
+        return uint256(price * 1e10); // 1**10 == 10000000000
     }
 
     function getVersion() public view returns (uint256) {
-        AggregatorV3Interface aggreIn = AggregatorV3Interface(0x694AA1769357215DE4FAC081bf1f309aDC325306);
-        return aggreIn.version();
+        AggregatorV3Interface priceFeed = AggregatorV3Interface(
+            0x694AA1769357215DE4FAC081bf1f309aDC325306
+        );
+        return priceFeed.version();
     }
 
-    function getConversionRate() public {
-        // TODO
+    function getConversionRate(uint256 ethAmount)
+        public
+        view
+        returns (uint256)
+    {
+        uint256 ethPic = getPrice();
+        // Wei转为ETH
+        uint256 ethAmountInUsd = (ethPic * ethAmount) / 1e18;
+        return ethAmountInUsd;
     }
 
     // 提取资金
