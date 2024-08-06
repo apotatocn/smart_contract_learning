96da66caab5eed9bb625ff34f1d036c7a5e15d97
apotatocn <huxiaopeng666@qq.com>
2024-08-06 14:26:18 +0800

    添加提现代码

--- storage-factory/contracts/FundMe.sol
+++ storage-factory/contracts/FundMe.sol
@@ -7,14 +7,20 @@ import "./PriceConverter.sol";
 // Withdraw funds（提取资金）
 // Set a minimum funding value in USD（同时设置一个以 usd(美元)计价的最小资助额）
 contract FundMe {
-
     using PriceConverter for uint256;
 
-    uint256 public minimumUsd = 50;
+    uint256 public minimumUsd = 1;
     // 用户地址
     address[] public funders;
     // 用户地址 => (Wei/ETU)
     mapping(address => uint256) public addressToAointFunded;
+
+    // 合约部署人
+    address public owner;
+
+    constructor() {
+        owner = msg.sender;
+    }
 
     // 从用户处获得资金
     // 20000000000000000
@@ -36,7 +42,36 @@ contract FundMe {
         addressToAointFunded[msg.sender] = msg.value;
     }
 
-    
     // 提取资金
-    function withdraw() public {}
+    function withdraw() public onlyOwner {
+        for (
+            uint256 funderIndex = 0;
+            funderIndex < funders.length;
+            funderIndex++
+        ) {
+            address funder = funders[funderIndex];
+            addressToAointFunded[funder] = 0;
+        }
+        // reset the array
+        funders = new address[](0);
+        // actually withdraw funds
+        // // 1.transger
+        // payable(msg.sender).transfer(address(this).balance);
+        // // 2.send
+        // bool sendSuccess = payable(msg.sender).send(address(this).balance);
+        // require(sendSuccess, "Send failed");
+        // 3.call
+        (bool callSuccess, ) = payable(msg.sender).call{
+            value: address(this).balance
+        }("");
+        require(callSuccess, "Call failed");
+    }
+
+    modifier onlyOwner {
+        // 合约部署人校验
+        require(msg.sender == owner, "Not allowed!");
+        // _; 代表函数内的方法，如果_;在后面说明先执行（require(msg.sender == owner, "Not allowed!");）在执行修饰的函数
+        // 他就像是一个占位符一样，将我们要执行的函数内容进行占位
+        _;
+    }
 }
